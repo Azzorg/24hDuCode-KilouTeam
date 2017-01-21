e2704bcdd2e2e89c1aaa6aa6eea814c66ca65659
Azzorg <c.goupil72@gmail.com>
2017-01-21 17:24:36 +0100

    Création socket de tous les clients

--- src/application/Serveur.java
+++ src/application/Serveur.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Serveur {
 	private static int PORT = 1500;
@@ -9,6 +14,28 @@ public class Serveur {
 	public static void main(String[] args) {
 		try {
 			ServerSocket welcomeSocket = new ServerSocket(PORT);
+			List<Socket> listClientSocket = new ArrayList<Socket>();
+			List<BufferedReader> listClientBufferedReader = new ArrayList<BufferedReader>();
+			int nbJoueur;
+			
+			while(true){
+				//Accepte le premier client et l'ajoute à la liste des clients
+				listClientSocket.add(welcomeSocket.accept());
+				
+				//Création du BufferedReader du client1
+				listClientBufferedReader.add(new BufferedReader(new InputStreamReader(listClientSocket.get(0).getInputStream())));
+				
+				//Récupération du nombre de joueur qu'il aura dans la partie
+				nbJoueur = Integer.parseInt(listClientBufferedReader.get(0).readLine());
+				
+				//Attente de tous les joueurs et création de leur socket
+				for(int i = 0; i<nbJoueur-1; i++){
+					System.out.println("new player added to the server");
+					listClientSocket.add(welcomeSocket.accept());
+				}
+				
+				
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
