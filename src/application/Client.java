3655997f01603706887335fc46892f5d7c69d920
GERVIER <r.gervier@orange.fr>
2017-01-22 00:35:10 +0100

    Attention !

--- src/application/Client.java
+++ src/application/Client.java
@@ -4,33 +4,20 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-
 public class Client extends Application{
-
-	private Scene scene;
-	
-	@FXML
-	AnchorPane browerEnplacement;
 	
 	@Override
 	public void start(Stage stage) throws IOException {
 		// create the scene
-		
-		
-		Parent root = FXMLLoader.load(getClass().getResource("../inter/test.fxml"));
+
+		Parent root = FXMLLoader.load(getClass().getResource("../View/test.fxml"));
 		Scene scene = new Scene(root);
-		
-		
-		System.out.println(browerEnplacement);
-		
-		
+
 		stage.setTitle("Web View");
 		stage.setScene(scene);
 		stage.show();
