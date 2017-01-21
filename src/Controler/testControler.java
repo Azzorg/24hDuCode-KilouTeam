a5d4f0a2d919b3c35f5c5db232c05435bb44995b
GERVIER <r.gervier@orange.fr>
2017-01-22 00:39:04 +0100

    r

--- src/Controler/testControler.java
+++ src/Controler/testControler.java
@@ -5,15 +5,18 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class testControler implements Initializable{
 
 	@FXML
 	WebView MapView;
-
+	 final String htmlUrl = "C:/Users/Rémy/git/24hDuCode-KilouTeam/resources/site/index2.html";
+	
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+		WebEngine webEngine = MapView.getEngine();
+		webEngine.load("file:///"+htmlUrl);
 	}
 }
