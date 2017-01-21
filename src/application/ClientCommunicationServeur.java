c547bd5a82310a5cca615a3dae2b6b359402766f
Azzorg <c.goupil72@gmail.com>
2017-01-21 22:53:45 +0100

    Début de la partie jeu côté serveur

--- src/application/ClientCommunicationServeur.java
+++ src/application/ClientCommunicationServeur.java
@@ -1,7 +1,90 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ClientCommunicationServeur {
-	public static void main(String args[]){
-		
+	private int PORT = 1500;
+
+	public static void main(String args[]) {
+		try {
+			//Nombre de joueur
+			int nbJoueur = 2;
+			
+			//Création de deux clients
+			ClientCommunicationServeur client1 = new ClientCommunicationServeur();
+			ClientCommunicationServeur client2 = new ClientCommunicationServeur();
+
+			//Le premier client tente de se connecter au serveur
+			Socket socketClient1 = new Socket("localhost", client1.PORT);
+			
+			System.out.println("Connection client 1 : OK");
+			
+			//Création de in et out pour le client 1
+			BufferedReader client1In = new BufferedReader(new InputStreamReader(socketClient1.getInputStream()));
+			PrintStream client1Out = new PrintStream(socketClient1.getOutputStream());
+			
+			//Attente que le serveur soit OK
+			while(!client1In.readLine().equals("OK")){
+				System.out.println("Serveur pas OK");
+			}
+			
+			//Envoi au serveur du nombre de 
+			client1Out.println(nbJoueur);
+			
+			System.out.println("Envoi du nombre de joueur");
+			
+			//Attente que le serveur soit OK
+			while(!client1In.readLine().equals("OK")){
+				System.out.println("Serveur pas OK");
+			}
+			
+			System.out.println("Client 1 OK");
+			
+			//Le deuxième client se connecte au serveur
+			Socket socketClient2 = new Socket("localhost", client2.PORT);
+			
+			System.out.println("Connection client 2 : OK");
+			
+			//Création de in et out pour le client 1
+			BufferedReader client2In = new BufferedReader(new InputStreamReader(socketClient2.getInputStream()));
+			PrintStream client2Out = new PrintStream(socketClient2.getOutputStream());
+
+			//Attente que le serveur soit OK
+			while(!client2In.readLine().equals("OK")){
+				System.out.println("Serveur pas OK");
+			}
+			
+			System.out.println("Client 2 OK");
+			
+			//Client 1 attend le début de la partie
+			while(!client1In.readLine().equals("DEBUT")){
+				System.out.println("Client 1 : Attente du début de la partie");
+			}
+			client1Out.println("OK");
+			
+			System.out.println("Client 1  début ok");
+			
+			//Client 2 attend le début de la partie
+			while(!client2In.readLine().equals("DEBUT")){
+				System.out.println("Client 2 : Attente du début de la partie");
+			}
+			client2Out.println("OK");
+
+			System.out.println("Client 2 début ok");
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/application/Serveur.java
+++ src/application/Serveur.java
@@ -20,7 +20,7 @@ public class Serveur {
 	 */
 	public static void waitConfirmClient(List<Socket> list, int nbJoueur) throws InterruptedException{
 		//Tableau de Thread pour les attentes de confirmation des clients
-		ThreadOK threadOK [] = new ThreadOK[nbJoueur];;
+		ThreadOK threadOK [] = new ThreadOK[nbJoueur];
 		
 		for(int i = 0; i<nbJoueur; i++){
 			threadOK[i] = new ThreadOK(list.get(i));
@@ -36,6 +36,7 @@ public class Serveur {
 		try {
 			System.out.println("début");
 			ServerSocket welcomeSocket = new ServerSocket(PORT);
+			boolean fin;
 			//Liste des sockets des clients
 			List<Socket> listClientSocket = new ArrayList<Socket>();
 			//Liste des BufferedReader des clients -> in
@@ -43,12 +44,13 @@ public class Serveur {
 			//Liste des PrintStream des clients -> out 
 			List<PrintStream> listClientPrintStream = new ArrayList<PrintStream>();
 			
-			
-			
-			int nbOk = 0;
+			int joueurTour = 0;
 			int nbJoueur = 0;
 			
 			while(true){
+				fin = false;
+				joueurTour = -1;
+				nbJoueur = 0;
 				//Accepte le premier client et l'ajoute à la liste des clients
 				listClientSocket.add(welcomeSocket.accept());
 				
@@ -56,32 +58,79 @@ public class Serveur {
 				listClientBufferedReader.add(new BufferedReader(new InputStreamReader(listClientSocket.get(0).getInputStream())));
 				listClientPrintStream.add(new PrintStream(listClientSocket.get(0).getOutputStream()));
 				
+				//Envoi au client 1 que c'est ok
+				listClientPrintStream.get(0).println("OK");
+				
 				//Récupération du nombre de joueur qu'il aura dans la partie
 				nbJoueur = Integer.parseInt(listClientBufferedReader.get(0).readLine());
+
+				//Envoi au client 1 que c'est ok
+				listClientPrintStream.get(0).println("OK");
 				
 				//Attente de tous les joueurs et création de leur socket
 				for(int i = 1; i<nbJoueur; i++){
 					System.out.println("new player added to the server");
 					listClientSocket.add(welcomeSocket.accept());
+					System.out.println("Player added");
+					
+					//System.out.println("Envoie au client" + i+1 + " : OK");
+					
 					listClientBufferedReader.add(new BufferedReader(new InputStreamReader(listClientSocket.get(i).getInputStream())));
 					listClientPrintStream.add(new PrintStream(listClientSocket.get(i).getOutputStream()));
+
+					//Envoi au client 1 que c'est ok
+					listClientPrintStream.get(i).println("OK");
 				}
 				
 				//Envoie à tout le monde que la partie commence
-				for(PrintStream out : listClientPrintStream){
+				/*for(PrintStream out : listClientPrintStream){
 					System.out.println("Dis au clients que le jeu démarre");
 					out.println("DEBUT");
+				}*/
+				
+				//Envoi au client 1 que c'est le début de la partie
+				listClientPrintStream.get(0).println("DEBUT");
+				while(!listClientBufferedReader.get(0).readLine().equals("OK")){
+					System.out.println("Client 1 pas OK");
 				}
 				
+				//Envoi au client 2 que c'est le début de la partie
+				listClientPrintStream.get(1).println("DEBUT");
+				while(!listClientBufferedReader.get(1).readLine().equals("OK")){
+					System.out.println("Client 2 pas OK");
+				}
+				
+				System.out.println("Début du game");
+				
 				//Attend que tous les clients confirme que la partie commence
-				waitConfirmClient(listClientSocket, nbJoueur);
+				//waitConfirmClient(listClientSocket, nbJoueur);
+				
+				//Début du jeu
+				while(!fin){
+					joueurTour++;
+					
+					//Désignation du joueur qui doit jouer
+					for(int i = 0; i<nbJoueur; i++){
+						//Envoi de "YOURTURN" au joueur qui doit jouer
+						if(i == joueurTour%nbJoueur)
+							listClientPrintStream.get(i).println("YOURTURN");
+						//Envoi de "NOTYOURTURN" aux joueurs qui ne jouent pas + de l'id du joueur qui joue
+						else
+							listClientPrintStream.get(i).println("NOTYOURTURN\n" + joueurTour%nbJoueur);
+					}
+					
+					
+					
+					
+				}
+				
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InterruptedException e) {
+		} /*catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 
 	}
 
