753458c38e7fa803a175c55f6b22067f926e047d
GERVIER <r.gervier@orange.fr>
2017-01-22 02:06:04 +0100

    Commit

--- src/Controler/gameControler.java
+++ src/Controler/gameControler.java
@@ -9,18 +9,33 @@ import javafx.scene.image.ImageView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-public class testControler implements Initializable{
+public class gameControler implements Initializable{
+
+	@FXML
+	ImageView Card_1;
+	@FXML
+	ImageView Card_2;
+	@FXML
+	ImageView Card_3;
+	@FXML
+	ImageView Card_4;
+	@FXML
+	ImageView Card_5;
+	@FXML
+	ImageView Card_6;
 
 	@FXML
 	WebView MapView;
 	final String htmlUrl = "C:/Users/Rémy/git/24hDuCode-KilouTeam/resources/site/index2.html";
 	
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		WebEngine webEngine = MapView.getEngine();
 		webEngine.load("file:///"+htmlUrl);
 		System.out.println(htmlUrl);
-		
-		ImageView i = new ImageView();
+		System.err.println(Card_1);
 	}
+
+	
 }
